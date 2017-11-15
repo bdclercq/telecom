9fc78d8e8cf8e11cb1b88cb46983f258905b32c9
Student <bdclercq@hotmail.com>
2017-11-15 14:22:54 +0100

    update

---
+++ click/elements/local/RegReqEncap.cc
@@ -0,0 +1,119 @@
+#include <click/config.h>
+#include <click/confparse.hh>
+#include <click/error.hh>
+#include <click/args.hh>
+#include <click/vector.cc>
+#include <clicknet/ip.h>
+#include <clicknet/ether.h>
+#include <click/glue.hh>
+#include <click/standard/alignmentinfo.hh>
+
+#include "RegReqEncap.hh"
+CLICK_DECLS
+
+struct RegReqPacket {
+
+    uint8_t type;
+    uint8_t flags;
+    uint16_t lifetime;
+    uint32_t home_address;
+    uint32_t home_agent;
+    uint32_t care_of_address;
+    uint64_t identification;
+
+};
+
+RegReq::RegReq()
+{
+}
+
+RegReq::~RegReq()
+{
+}
+
+int
+RegReq::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+    //bool do_cksum = true;
+    //_interval = 0;
+    if (Args(conf, this, errh)
+	.read_mp("TYPE", _regreq_type)
+	.read_mp("S", _s_flag)
+	.read_mp("B", _b_flag)
+	.read_mp("D", _d_flag)
+	.read_mp("M", _m_flag)
+    .read_mp("G", _g_flag)
+    .read_mp("r", _r_flag)
+    .read_mp("T", _t_flag)
+    .read_mp("x", _x_flag)
+    .read_mp("LIFETIME", _lifetime)
+    .read_mp("HOME_ADDRESS", _haddr)
+    .read_mp("HOME_AGENT", _haaddr)
+    .read_mp("CARE_OF_ADDRESS", _coaddr)
+    .read_mp("IDENTIFCATION", _identification)
+	.complete() < 0)
+	return -1;
+
+  //_id = 0;
+  //_cksum = do_cksum;
+  //_count = 0;
+
+#ifdef CLICK_LINUXMODULE
+  // check alignment
+  {
+    int ans, c, o;
+    ans = AlignmentInfo::query(this, 0, c, o);
+    _aligned = (ans && c == 4 && o == 0);
+    if (!_aligned)
+      errh->warning("IP header unaligned, cannot use fast IP checksum");
+    if (!ans)
+      errh->message("(Try passing the configuration through `click-align'.)");
+  }
+#endif
+
+  return 0;
+}
+
+Packet* RegReq::make_packet(){
+
+    int headroom = sizeof(click_ether);
+    WritablePacket* q = Packet::make(headroom, 0, 160, 0);
+    if (!q)
+        return 0;
+    memset(q->data(), '\0', 160);
+    
+    Vector<bool> flags;
+	flags.push_back(_s_flag);
+	flags.push_back(_b_flag);
+	flags.push_back(_d_flag);
+	flags.push_back(_m_flag);
+	flags.push_back(_g_flag);
+	flags.push_back(_r_flag);
+	flags.push_back(_t_flag);
+	flags.push_back(_x_flag);
+    
+    RegReqPacket* rr = (RegReqPacket*)q->data();
+    
+    rr->type = _regreq_type;
+    
+    uint8_t flagint = 1;
+    for (int i = 0; i < flags.size(); i++) {
+        if (flags[i]) {
+            flagint += (2 ** ((flags.size() - 1) - i));
+        }
+    }
+    
+    rr->flags = flagint;
+    rr->lifetime = _lifetime;
+    rr->home_address = _haddr;
+    rr->home_agent = _haaddr;
+    rr->care_of_address = _coaddr;
+    rr->identification = _identicifation;
+    
+    return q;
+
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(RegReq)
+
---
+++ click/elements/local/RegReqEncap.hh
@@ -0,0 +1,55 @@
+#ifndef CLICK_REGREQ_HH
+#define CLICK_REGREQ_HH
+
+#include <click/element.hh>
+#include <clicknet/udp.h>
+#include <click/atomic.hh>
+CLICK_DECLS
+
+class RegReq : public Element {
+  private:
+  struct in_addr _saddr;
+  struct in_addr _daddr;
+  
+  uint16_t _sport;
+  uint16_t _dport;
+  
+  struct in_addr _haddr;
+  struct in_addr _haaddr;
+  struct in_addr _coaddr;
+  
+  uint64_t _identification;
+  
+  uint8_t _regreq_type;
+  uint16_t _lifetime;
+  
+  bool _s_flag;
+  bool _b_flag;
+  bool _d_flag;
+  bool _m_flag;
+  bool _g_flag;
+  bool _r_flag;
+  bool _t_flag;
+  bool _x_flag;
+  
+  #ifdef CLICK_LINUXMODULE
+  bool _aligned : 1;
+  #endif
+
+ public:
+
+  RegReq() CLICK_COLD;
+  ~RegReq() CLICK_COLD;
+
+  const char *class_name() const	{ return "RegReq"; }
+  const char *port_count() const	{ return PORTS_1_1; }
+  const char *flags() const		{ return "A"; }
+
+  int configure(Vector<String> &, ErrorHandler *) CLICK_COLD;
+
+  Packet *make_packet();
+
+};
+CLICK_ENDDECLS
+
+#endif
